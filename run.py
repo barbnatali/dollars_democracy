3e92e00d72f37301dc8e448176180356caddc243
Barb Natali <barb.natali@gmail.com>
2013-08-13 23:44:50 -0700

    Sidebar info

--- run.py
+++ run.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
-import shelve
-from math import ceil
 from flask import redirect
 from flask import request, url_for
-from collections import OrderedDict
-import xmltodict
 import requests
-import unicodedata
+import urllib2  
 import keys
 
 # opensecrets 
@@ -43,19 +39,33 @@ app = Flask(__name__)
 def format_currency(value):
 	value = float(value)
 	return "${:,.2f}".format(value)
+
+
+# Get Super Pac Expenditures from OpenSecrets widget
+def superpac_money():	
+	url = 'http://www.opensecrets.org/widgets/outsidespending2012.js'
+	url = urllib2.urlopen(url).read()
+	url_split = url.split()
+	length = len(url_split)
+	amount = url_split[length-1]
+	# removes quotes and semicolon
+	amount = amount[1:-2]
+	return amount
     
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	superpac = superpac_money()
+	return render_template('index.html', superpac=superpac)
 
 
 #http://127.0.0.1:5000/campaign/pelosi
 @app.route('/campaign/')
 @app.route('/campaign/<politician>')
 def campaign(politician=None):
+	superpac = superpac_money()
 	if politician == None:
-		return render_template('choose_location.html')
+		return render_template('choose_location.html', superpac=superpac)
 	else:
 		# congress
 		# politician_info = congress.legislators(lastname=politician)[0]
@@ -68,11 +78,12 @@ def campaign(politician=None):
 		politician_info = ie.entities.search(politician)[0]
 		id = politician_info['id']
 		top_industries = ie.pol.industries(id, limit=10)
-		return render_template('campaign.html', politician=politician_info, top_industries=top_industries)
+		return render_template('campaign.html', politician=politician_info, top_industries=top_industries, superpac=superpac)
 	
 	
 @app.route('/choose_politician')
 def choose_politician():
+	superpac = superpac_money()
 	if 'location' in request.args:
 		location = request.args.get('location', '')
 	else:
@@ -86,31 +97,35 @@ def choose_politician():
 	else:
 		# openstates
 		legislators = openstates.legislators(state=location)
-	return render_template('choose_politician.html', politicians=legislators)
+	return render_template('choose_politician.html', politicians=legislators, superpac=superpac)
 	
 
 @app.route('/lobbying')
 def lobbying():
+	superpac = superpac_money()
 	#top_10 = ie.entities.top_n_lobbyist_bundlers(limit=10)
 	top_orgs = ie.entities.top_n_organizations(limit=15, cycle='-1')
 	top_industries = ie.entities.top_n_industries(limit=15)
-	return render_template('lobbying.html', top_orgs=top_orgs, top_industries=top_industries)
+	return render_template('lobbying.html', top_orgs=top_orgs, top_industries=top_industries, superpac=superpac)
 
 
 @app.route('/organizations')
 def organizations():
-	return render_template('organizations.html')
+	superpac = superpac_money()
+	return render_template('organizations.html', superpac=superpac)
 	
 	
 @app.route('/super_pacs')
 def super_pacs():
+	superpac = superpac_money()
 	# TODO - get dynamic data for 2014
-	return render_template('super_pacs.html')
+	return render_template('super_pacs.html', superpac=superpac)
 	
 	
 @app.route('/take_action')
 def take_action():
-	return render_template('take_action.html')
+	superpac = superpac_money()
+	return render_template('take_action.html', superpac=superpac)
 
 
 if __name__ == '__main__':
