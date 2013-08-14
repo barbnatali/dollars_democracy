5b8c1061d9046e5b091fa204b46e84f33b164bcc
Barb Natali <barb.natali@gmail.com>
2013-08-14 01:39:43 -0700

    Added state and federal politicians to choose politician page. Added favicon.

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import redirect
 from flask import request, url_for
+from pyzipcode import ZipCodeDatabase
 import requests
 import urllib2  
 import keys
@@ -41,7 +42,7 @@ def format_currency(value):
 	return "${:,.2f}".format(value)
 
 
-# Get Super Pac Expenditures from OpenSecrets widget
+# Get Amount of Super Pac Expenditures from OpenSecrets widget
 def superpac_money():	
 	url = 'http://www.opensecrets.org/widgets/outsidespending2012.js'
 	url = urllib2.urlopen(url).read()
@@ -51,21 +52,25 @@ def superpac_money():
 	# removes quotes and semicolon
 	amount = amount[1:-2]
 	return amount
-    
+   
+   
+def get_state_from_zip(zip): 
+	z = ZipCodeDatabase()
+	zipcode = z[zip]
+	return zipcode.state
+	
 
 @app.route('/')
 def index():
-	superpac = superpac_money()
-	return render_template('index.html', superpac=superpac)
+	return render_template('index.html')
 
 
 #http://127.0.0.1:5000/campaign/pelosi
 @app.route('/campaign/')
 @app.route('/campaign/<politician>')
 def campaign(politician=None):
-	superpac = superpac_money()
 	if politician == None:
-		return render_template('choose_location.html', superpac=superpac)
+		return render_template('choose_location.html')
 	else:
 		# congress
 		# politician_info = congress.legislators(lastname=politician)[0]
@@ -78,54 +83,51 @@ def campaign(politician=None):
 		politician_info = ie.entities.search(politician)[0]
 		id = politician_info['id']
 		top_industries = ie.pol.industries(id, limit=10)
-		return render_template('campaign.html', politician=politician_info, top_industries=top_industries, superpac=superpac)
+		return render_template('campaign.html', politician=politician_info, top_industries=top_industries)
 	
 	
 @app.route('/choose_politician')
 def choose_politician():
-	superpac = superpac_money()
 	if 'location' in request.args:
 		location = request.args.get('location', '')
 	else:
-		location = 'ca'
- 	if location.isdigit():
- 		# Congress API doesn't have as many entries
- 		url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+str(keys.sunlight_key)+'&zip='+str(location)
- 		r = requests.get(url)
- 		r = r.json()
- 		legislators = r['results']
-	else:
-		# openstates
-		legislators = openstates.legislators(state=location)
-	return render_template('choose_politician.html', politicians=legislators, superpac=superpac)
+		location = '91101'
+	state = get_state_from_zip(location)
+
+	# Congress API doesn't have as many entries
+	url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey='+str(keys.sunlight_key)+'&zip='+str(location)
+	r = requests.get(url)
+	r = r.json()
+	congress = r['results']
+	
+	# openstates
+	legislators = openstates.legislators(state=state)
+				
+	return render_template('choose_politician.html', legislators=legislators, congress=congress)
 	
 
 @app.route('/lobbying')
 def lobbying():
-	superpac = superpac_money()
 	#top_10 = ie.entities.top_n_lobbyist_bundlers(limit=10)
 	top_orgs = ie.entities.top_n_organizations(limit=15, cycle='-1')
 	top_industries = ie.entities.top_n_industries(limit=15)
-	return render_template('lobbying.html', top_orgs=top_orgs, top_industries=top_industries, superpac=superpac)
+	return render_template('lobbying.html', top_orgs=top_orgs, top_industries=top_industries)
 
 
 @app.route('/organizations')
 def organizations():
-	superpac = superpac_money()
-	return render_template('organizations.html', superpac=superpac)
+	return render_template('organizations.html')
 	
 	
 @app.route('/super_pacs')
 def super_pacs():
-	superpac = superpac_money()
 	# TODO - get dynamic data for 2014
-	return render_template('super_pacs.html', superpac=superpac)
+	return render_template('super_pacs.html')
 	
 	
 @app.route('/take_action')
 def take_action():
-	superpac = superpac_money()
-	return render_template('take_action.html', superpac=superpac)
+	return render_template('take_action.html')
 
 
 if __name__ == '__main__':
